ca28d677e9eadaeb483b3dfeb3e230dd3eaed641
koopReynders <mail@kopo.nl>
2016-01-31 21:28:30 +0100

    links

--- nerd.php
+++ nerd.php
@@ -35,29 +35,30 @@ $talk = "[".$command."] \n";
 switch($command){
     case "TODO":
 		$talk .= "Next up is: Kickoff! \n";
-		$talk .= "Monday 1 Feb. Be there at 10.";
+		$talk .= "Maandag 1 Februari in de N-Lounge. Zorg dat je er om 10 bent. \n";
+        $talk .= "Zorg dat je een fiets of OV kaart meeneemt. We gaan in de ochtend op pad. Wat heb je nog meer nodig? 3G, je laptop en een fineliner. \n";
 		break;
     case "SCHEDULE":
     case "SCHEMA":
-		$talk .= "0. 	Monday 1 Feb	Kickoff bedrijven bezoeken (Voorhoede, GRRR, Looklive, Q42) @ N-gebouw \n";
-		$talk .= "1. 	Tuesday 2 Feb 	Arjan Westerdiep (Drububu) - Nerd Alert @ Crea \n";
-		$talk .= "2. 	Tuesday 9 Feb 	Marije Schaake (Eend) - Components @ Crea \n";
-		$talk .= "3. 	Tuesday 16 Feb 	 \n";
-		$talk .= "4. 	Tuesday 23 Feb 	 \n";
-		$talk .= "5. 	Tuesday 8 Mrt 	 \n";
-		$talk .= "6. 	Tuesday 15 Mrt 	Peter Paul Koch - the Viewport @ Crea \n";
-		$talk .= "7. 	Tuesday 22 Mrt 	Mattijs Bliek (GRRR) - Functionele animatie @ GRRR \n";
-		$talk .= "8. 	Tuesday 29 Mrt 	 \n";
-		$talk .= "9. 	Tuesday 5 Apr 	 \n";
-		$talk .= "10. 	Tuesday 12 Apr 	 \n";
-		$talk .= "11. 	Tuesday 19 Apr 	 \n";
-		$talk .= "12. 	Tuesday 26 Apr 	 \n";
-		$talk .= "13. 	Tuesday 10 Mei 	Github  @ Github \n";
-		$talk .= "14. 	Tuesday 17 Mei 	 \n";
-		$talk .= "15. 	Tuesday 24 Mei 	 \n";
-		$talk .= "16. 	Tuesday 31 Mei 	 \n";
-		$talk .= "17. 	Tuesday 7 Jun 	 \n";
-		$talk .= "18. 	Tuesday 14 Jun 	 \n";
+		$talk .= "0. 	Maandag 1 Feb	Kickoff & bedrijven bezoeken (Voorhoede www.voorhoede.nl, GRRR www.grrr.nl, Looklive www.looklive.nl, Q42 www.q42.nl) @ N-gebouw \n";
+		$talk .= "1. 	Dinsdag 2 Feb 	Arjan Westerdiep (Drububu) - Nerd Alert @ Crea \n";
+		$talk .= "2. 	Dinsdag 9 Feb 	Marije Schaake (Eend) - Components @ Crea \n";
+		$talk .= "3. 	Dinsdag 16 Feb 	 \n";
+		$talk .= "4. 	Dinsdag 23 Feb 	 \n";
+		$talk .= "5. 	Dinsdag 8 Mrt 	 \n";
+		$talk .= "6. 	Dinsdag 15 Mrt 	Peter Paul Koch - the Viewport @ Crea \n";
+		$talk .= "7. 	Dinsdag 22 Mrt 	Mattijs Bliek (GRRR) - Functionele animatie @ GRRR \n";
+		$talk .= "8. 	Dinsdag 29 Mrt 	 \n";
+		$talk .= "9. 	Dinsdag 5 Apr 	 \n";
+		$talk .= "10. 	Dinsdag 12 Apr 	 \n";
+		$talk .= "11. 	Dinsdag 19 Apr 	 \n";
+		$talk .= "12. 	Dinsdag 26 Apr 	 \n";
+		$talk .= "13. 	Dinsdag 10 Mei 	Github  @ Github \n";
+		$talk .= "14. 	Dinsdag 17 Mei 	 \n";
+		$talk .= "15. 	Dinsdag 24 Mei 	 \n";
+		$talk .= "16. 	Dinsdag 31 Mei 	 \n";
+		$talk .= "17. 	Dinsdag 7 Jun 	 \n";
+		$talk .= "18. 	Dinsdag 14 Jun 	 \n";
 		break;
 	case "12345":
 		$talk .= "Hey, ".$user_name." you've finished the first Weekly Nerd. Good for you! \n";
@@ -71,8 +72,9 @@ switch($command){
 		//$talk .= "To code or not to code, that is the question. \n\n";
 		$talk .= "You have to decide for yourself if you take the blue or the red pill ... \n";
         $talk .= " - Try the command 'todo' for the next scheduled event. \n";
+        $talk .= " - Try the command 'todo' and add your name  \n";
         $talk .= " - Ask for the 'schedule' if you want to know all the Weekly Nerd talks. \n";
-        $talk .= " - After a talk, enter your 5 digit code for your next assignment. (test with 12345) \n";
+        //$talk .= " - After a talk, enter your 5 digit code for your next assignment. (test with 12345) \n";
 		break;
     default:
        if($user_text == "what" || $user_text == "what?" || $user_text == "wat?" || $user_text == "wat" || $user_text == "wha" || $user_text == "wa"){
