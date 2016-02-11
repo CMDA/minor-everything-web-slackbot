475ea9e9c2545c22f812ce9fe42b685b678f1929
koopReynders <mail@kopo.nl>
2016-02-11 14:27:49 +0100

    nerd update WN#3

--- nerd.php
+++ nerd.php
@@ -33,11 +33,6 @@ $command = strtoupper($_POST["text"]);
 $talk = "";
 //$talk = "[".$command."] \n";
 
-$bezoek = "";
-$bezoek .= "De bedoeling van dit bezoek is dat jullie alvast zicht krijgen op wat frontenders zoal doen, hoe dit soort bedrijven werken en wat voor opdrachten ze krijgen. ";
-$bezoek .= "En om geïnspireerd te raken natuurlijk. Na de minor Web Development zou je stage bij zo'n type bedrijf kunnen gaan lopen. \n";
-$bezoek .= "\n";
-$bezoek .= "Veel plezier. Om 13:00 gaan we lunchen in de N-Lounge. ";
 
 switch($command){
     case "TODO":
@@ -54,7 +49,6 @@ switch($command){
 		$talk .= "To complete the first assignment, write a (short) blog post about the talk ";
 		$talk .= "and follow up on 2 or 3 links the speaker discussed. \n";
 		$talk .= "- This was the first Weekly Nerd (1/18) \n";
-
 		echo $talk;
 		break;
 
--- todo.php
+++ todo.php
@@ -23,153 +23,176 @@ $talk = "";
 //$talk = "[".$command."] \n";
 */
 
-//WEEKLY NERD #1
-$talk = "";
-$talk .= "Dinsdag 2 februari om 16:00 is de eerste Weekly Nerd (ergens) in het N-Gebouw. \n";
-$talk .= "*** Arjan Westerdiep is freelance frontend developer. Sinds 2003 maakt hij onder de naam Drububu technische en visuele online kunstwerken, pixel art, gekke dingen en commerciële opdrachten.";
-
-echo $talk;
 
 
-/*
+//WEEKLY NERD #3 - Donny @ LOOKLIVE
+$wn3 = "";
+$wn3 .= "Dinsdag 16 februari gaan we naar Looklive. Je wordt om 16:00 verwacht op hun kantoor aan de Prinsengracht 697 BG-A. \n";
+$wn3 .= "Geen vervelende lokatie daar aan de gracht, bovendien zul je ter plekke worden ontvangen door onze eigen Donny Wals! Dinsdag gaat hij wat vertellen over hun workflow, task manager en Git. \n";
+
+$wn3 .= "*** Donny Wals werkt als frontender bij LookLive, een e-commerce bedrijf met een eigen content & development afdeling. Hij heeft vroeger CMD gestudeerd en komt in blok 2 ook lesgeven in het vak Performance Matters. ";
+$talk = $wn3;
+
+
+
+//WEEKLY NERD #2 - EEND
+$wn2 = "";
+$wn2 .= "Dinsdag 9 februari om 16:00 komt Marrije Schaake van Eend langs voor een workshop Responsive Content Design. De tweede Weekly Nerd is (ergens) in het N-Gebouw. \n";
+$wn2 .= "*** Marrije Schaake is Informatie Architect, knipt graag papier, speelt met Arduino's en is zakelijk leider van Eend.";
+//$talk = $wn2;
+
+
+//WEEKLY NERD #1 - DRUBUBU
+$wn1 = "";
+$wn1 .= "Dinsdag 2 februari om 16:00 is de eerste Weekly Nerd (ergens) in het N-Gebouw. \n";
+$wn1 .= "*** Arjan Westerdiep is freelance frontend developer. Sinds 2003 maakt hij onder de naam Drububu technische en visuele online kunstwerken, pixel art, gekke dingen en commerciële opdrachten.";
+//$talk = $wn1;
+
+
+
 //WEEKLY NERD #0 - Kickoff 1 feb
+$wn0 = "";
+$wn0 .= "Next up is: Kickoff! \n";
+$wn0 .= "Maandag 1 Februari in de N-Lounge. Zorg dat je er om 10 bent. \n";
+$wn0 .= "Zorg ook dat je een fiets of OV kaart meeneemt. We gaan in de ochtend op pad. ";
+$wn0 .= "Wat heb je nog meer nodig? 3G, je laptop en een fineliner. \n";
+$wn0 .= "Als je achter 'todo' ook nog je naam typt, krijg je te zien wat jij zometeen gaat doen, dus: 'todo voornaam'";
+//$talk = $wn_kickoff;
+
+//WEEKLY NERD #0 - bedrijven bezoeken
 $bezoek = "";
 $bezoek .= "De bedoeling van dit bezoek is dat jullie alvast zicht krijgen op wat frontenders zoal doen, hoe dit soort bedrijven werken en wat voor opdrachten ze krijgen. ";
 $bezoek .= "En om geïnspireerd te raken natuurlijk. Na de minor Web Development zou je stage bij zo'n type bedrijf kunnen gaan lopen. \n";
 $bezoek .= "\n";
 $bezoek .= "Veel plezier. Om 13:00 gaan we lunchen in de N-Lounge. ";
 
-    case "TODO":
-		$talk .= "Next up is: Kickoff! \n";
-		$talk .= "Maandag 1 Februari in de N-Lounge. Zorg dat je er om 10 bent. \n";
-        $talk .= "Zorg ook dat je een fiets of OV kaart meeneemt. We gaan in de ochtend op pad. ";
-        $talk .= "Wat heb je nog meer nodig? 3G, je laptop en een fineliner. \n";
-        $talk .= "Als je achter 'todo' ook nog je naam typt, krijg je te zien wat jij zometeen gaat doen, dus: 'todo voornaam'";
-		break;
+/*
+case "TODO ROBERT":
+	$wn0 = "";
+	$wn0 .= "Aha ".$user_name." in da house! Maar welke is het? Spier of Steen? Maak je svp bekend met je achternaam ipv je voornaam. ";	
+break;
 
-	case "TODO ROBERT":
-        $talk .= "Aha ".$user_name." in da house! Maar welke is het? Spier of Steen? Maak je svp bekend met je achternaam ipv je voornaam. ";	
-		break;
-
-	case "TODO MICHELE":
-		$talk .= "Ja ja ja, ".$user_name.", We gaan dus niet naar Occhio. \n";
-		$talk .= "Ze zitten weliswaar vlakbij. En zijn met toffe mensen toffe dingen aan het doen. Maar Michele is te druk hé? Jammerrrr \n";
-		$talk .= "\n";
-		$talk .= $bezoek;
-		$talk .= "\n Alleen lunchen is ook goed hoor.";
-		break;
-		
-	case "TODO CASPER":
-	case "TODO FONS":
-	case "TODO JESPER":
-	case "TODO MATTHIAS":
-	case "TODO SEM":
-	case "TODO ROOS":
-	case "TODO LINDA":
-	case "TODO TIJS":
-	case "TODO KRIJN":
-	case "TODO DAVID":
-	case "TODO HARM":
-		$talk .= "Wauzers, ".$user_name.", zometeen ga jij een kijkje nemen bij de alleraardigste coole nerds (en designers) van GRRR. \n";
-		$talk .= "Jij gaat straks met een groepje van 8 collega's GRRR bezoeken. ";
-		$talk .= "Je krijgt daar om 11:30 een (korte) rondleiding van David Spreekmeester. De opper geek. \n";
-		$talk .= "Het adres van GRRR is: \n";
-		$talk .= "Sint Antoniesbreestraat 16 \n";
-		$talk .= "Tel 020 320 77 08 \n";
-		$talk .= "Niet zo ver hier vandaan, vlak bij de Nieuwmarkt. \n";
-		$talk .= "\n";
-		$talk .= "GRRR is een ontwerpstudio, ooit begonnen met een drietal Interaction Designers. En doorgegroeid naar een full-stack ontwerpbureau met een sterke technische basis. ";
-		$talk .= "De ontwerpers begrijpen techniek en de techneuten kunnen ontwerpen. Een goede match met onze opleiding en minor. Op dit moment lopen er 2 CMD'ers afstudeerstage. ";
-		$talk .= "Een van de talentvolle designers/developers van GRRR is Mattijs Bliek. Hij komt later in de minor langs voor een Weekly Nerd. Als hij niet te druk is, check dan even op zijn scherm wat hij eigenlijk aan het doen is ... \n";
-		$talk .= $bezoek;
-		break;
+case "TODO MICHELE":
+	$wn0 = "";
+	$wn0 .= "Ja ja ja, ".$user_name.", We gaan dus niet naar Occhio. \n";
+	$wn0 .= "Ze zitten weliswaar vlakbij. En zijn met toffe mensen toffe dingen aan het doen. Maar Michele is te druk hé? Jammerrrr \n";
+	$wn0 .= "\n";
+	$wn0 .= $bezoek;
+	$wn0 .= "\n Alleen lunchen is ook goed hoor.";
+break;
 	
-	case "TODO DYLAN":
-	case "TODO JAIMIE":
-	case "TODO JOSHUA":
-	case "TODO RAYMOND":
-	case "TODO SPIER":
-	case "TODO BART":
-	case "TODO ROVER":
-	case "TODO DONNY":
-		$talk .= "Hup hup hup, ".$user_name.", we gaan naar LookLive. \n";
-		$talk .= "Geen vervelende lokatie daar aan de gracht, bovendien zul je ter plekke worden ontvangen door onze eigen Donny Wals! Hij heeft vroeger CMD gestudeerd en komt in blok 2 ook lesgeven in de minor in het vak Performance Matters. \n";
-		$talk .= "Jij gaat straks met een groepje van 7 collega's LookLive bezoeken. ";
-		$talk .= "Je krijgt daar om 11:30 een (korte) rondleiding. \n";
-		$talk .= "Het adres van LookLive is: \n";
-		$talk .= "Prinsengracht 697 BG-A \n";
-		$talk .= "Stukje de bocht om en je bent er. \n";
-		$talk .= "\n";
-		$talk .= "LookLive is een e-commerce bedrijf met een eigen content & development afdeling. Donny werkt er als frontender. ";
-		$talk .= $bezoek;
-		break;
+case "TODO CASPER":
+case "TODO FONS":
+case "TODO JESPER":
+case "TODO MATTHIAS":
+case "TODO SEM":
+case "TODO ROOS":
+case "TODO LINDA":
+case "TODO TIJS":
+case "TODO KRIJN":
+case "TODO DAVID":
+case "TODO HARM":
+	$wn0 = "";
+	$wn0 .= "Wauzers, ".$user_name.", zometeen ga jij een kijkje nemen bij de alleraardigste coole nerds (en designers) van GRRR. \n";
+	$wn0 .= "Jij gaat straks met een groepje van 8 collega's GRRR bezoeken. ";
+	$wn0 .= "Je krijgt daar om 11:30 een (korte) rondleiding van David Spreekmeester. De opper geek. \n";
+	$wn0 .= "Het adres van GRRR is: \n";
+	$wn0 .= "Sint Antoniesbreestraat 16 \n";
+	$wn0 .= "Tel 020 320 77 08 \n";
+	$wn0 .= "Niet zo ver hier vandaan, vlak bij de Nieuwmarkt. \n";
+	$wn0 .= "\n";
+	$wn0 .= "GRRR is een ontwerpstudio, ooit begonnen met een drietal Interaction Designers. En doorgegroeid naar een full-stack ontwerpbureau met een sterke technische basis. ";
+	$wn0 .= "De ontwerpers begrijpen techniek en de techneuten kunnen ontwerpen. Een goede match met onze opleiding en minor. Op dit moment lopen er 2 CMD'ers afstudeerstage. ";
+	$wn0 .= "Een van de talentvolle designers/developers van GRRR is Mattijs Bliek. Hij komt later in de minor langs voor een Weekly Nerd. Als hij niet te druk is, check dan even op zijn scherm wat hij eigenlijk aan het doen is ... \n";
+	$wn0 .= $bezoek;
+break;
 
-	case "TODO STEEN":
-	case "TODO DENNIS":
-	case "TODO JOB":
-	case "TODO MELVIN":
-	case "TODO SENNY":
-	case "TODO WESLEY":
-	case "TODO HELEEN":
-	case "TODO MAAIKE":
-	case "TODO RAHUL":
-	case "TODO VASILIS":
-		$talk .= "Aloha, ".$user_name.", jij gaat vandaag naar Q42. \n";
-		$talk .= "Jij gaat straks met een groepje van 8 collega's Q42 bezoeken. ";
-		$talk .= "Je krijgt daar om 11:30 een (korte) rondleiding van Guido Bouman.";
-		$talk .= "Het adres van Q42 is: \n";
-		$talk .= "Oostelijke Handelskade 749 \n";
-		$talk .= "Tel HQ 070 445 2342 \n";
-		$talk .= "Hun Amsterdamse kantoor ligt een kwartiertje fietsen van het N-Gebouw, vlak bij het IJ. \n";
-		$talk .= "\n";
-		$talk .= "Q42 zijn Nerds. Dat zeggen ze zelf. Vanuit (web) technologie benaderen ze ontwerp-problemen. En dat doen ze met succes. Maar ook met plezier. ";
-		$talk .= "Zoals ook te zien is aan hun gehackte koffiemachine, o.a. gemaakt door Guido. MIsschien kan hij daar tijdens een Weekly Nerd een keer meer over komen vertellen? Vraag even of hij daar zin in heeft ... ";
-		$talk .= "We gaan in de minor een aantal keren horen van Q42. In het vak Browser Technologies en Real-Time web komen ze langs, en in projectweek 3 krijgen jullie een uitdagende case voorgelegd met IoT & Meteor. \n";
-		$talk .= $bezoek;
-		break;
+case "TODO DYLAN":
+case "TODO JAIMIE":
+case "TODO JOSHUA":
+case "TODO RAYMOND":
+case "TODO SPIER":
+case "TODO BART":
+case "TODO ROVER":
+case "TODO DONNY":
+	$wn0 = "";
+	$wn0 .= "Hup hup hup, ".$user_name.", we gaan naar LookLive. \n";
+	$wn0 .= "Geen vervelende lokatie daar aan de gracht, bovendien zul je ter plekke worden ontvangen door onze eigen Donny Wals! Hij heeft vroeger CMD gestudeerd en komt in blok 2 ook lesgeven in de minor in het vak Performance Matters. \n";
+	$wn0 .= "Jij gaat straks met een groepje van 7 collega's LookLive bezoeken. ";
+	$wn0 .= "Je krijgt daar om 11:30 een (korte) rondleiding. \n";
+	$wn0 .= "Het adres van LookLive is: \n";
+	$wn0 .= "Prinsengracht 697 BG-A \n";
+	$wn0 .= "Stukje de bocht om en je bent er. \n";
+	$wn0 .= "\n";
+	$wn0 .= "LookLive is een e-commerce bedrijf met een eigen content & development afdeling. Donny werkt er als frontender. ";
+	$wn0 .= $bezoek;
+break;
 
-	case "TODO EMIEL":
-	case "TODO JARNO":
-	case "TODO LEANDER":
-	case "TODO MARTIJN":
-	case "TODO TOM":
-	case "TODO RIANNE":
-	case "TODO LISA":
-	case "TODO JOOST":
-	case "TODO LAURENS":
-		$talk .= "Zoooo, ".$user_name.", wat ga jij zometeen doen? \n";
-		$talk .= "Jij gaat straks met een groepje van 8 collega's de Voorhoede bezoeken. ";
-		$talk .= "Je krijgt daar om 11:30 een (korte) rondleiding van Femke Peterson. ";
-		$talk .= "Het adres van de Voorhoede is: \n";
-		$talk .= "Rijnsburgstraat 9 - 11 \n";
-		$talk .= "Tel 06 4897 9447 \n";
-		$talk .= "Hun kantoor ligt naast de Schinkel in de buurt van het Olympisch Stadion. \n";
-		$talk .= "\n";
-		$talk .= "De Voorhoede zijn Front-end developers die zich 100% op het web richten. Ze zijn gespecialiseerd in Web technologieën: Javascript, HTML & CSS. Een goede match voor onze minor! \n";
-		$talk .= "In projectweek 2 komt de Voorhoede jullie een uitdagende case voorleggen. ";
-		$talk .= "Vraag vooral ook nog even naar de opdracht die de Voorhoede heeft gedaan voor CommTrack. Over Web technologieën gesproken ...  \n";
-		$talk .= $bezoek;
-		break;
+case "TODO STEEN":
+case "TODO DENNIS":
+case "TODO JOB":
+case "TODO MELVIN":
+case "TODO SENNY":
+case "TODO WESLEY":
+case "TODO HELEEN":
+case "TODO MAAIKE":
+case "TODO RAHUL":
+case "TODO VASILIS":
+	$wn0 = "";
+	$wn0 .= "Aloha, ".$user_name.", jij gaat vandaag naar Q42. \n";
+	$wn0 .= "Jij gaat straks met een groepje van 8 collega's Q42 bezoeken. ";
+	$wn0 .= "Je krijgt daar om 11:30 een (korte) rondleiding van Guido Bouman.";
+	$wn0 .= "Het adres van Q42 is: \n";
+	$wn0 .= "Oostelijke Handelskade 749 \n";
+	$wn0 .= "Tel HQ 070 445 2342 \n";
+	$wn0 .= "Hun Amsterdamse kantoor ligt een kwartiertje fietsen van het N-Gebouw, vlak bij het IJ. \n";
+	$wn0 .= "\n";
+	$wn0 .= "Q42 zijn Nerds. Dat zeggen ze zelf. Vanuit (web) technologie benaderen ze ontwerp-problemen. En dat doen ze met succes. Maar ook met plezier. ";
+	$wn0 .= "Zoals ook te zien is aan hun gehackte koffiemachine, o.a. gemaakt door Guido. MIsschien kan hij daar tijdens een Weekly Nerd een keer meer over komen vertellen? Vraag even of hij daar zin in heeft ... ";
+	$wn0 .= "We gaan in de minor een aantal keren horen van Q42. In het vak Browser Technologies en Real-Time web komen ze langs, en in projectweek 3 krijgen jullie een uitdagende case voorgelegd met IoT & Meteor. \n";
+	$wn0 .= $bezoek;
+break;
 
-	case "TODO KOOP":
-		$talk .= "Geen dak boven je hoofd? Niet getreurd ".$user_name.", we gaan naar Funda. \n";
-		$talk .= "Je krijgt daar om 11:30 een (korte) rondleiding van Jeroen Mulder \n";
-		$talk .= "Het adres van Funda is: \n";
-		$talk .= "Piet Heinkade 167-173 \n";
-		$talk .= "tel 020 561 8200 \n";
-		$talk .= "Kwartiertje fitesen richting het IJ. \n";
-		$talk .= "\n";
-		$talk .= $bezoek;
-		break;
+case "TODO EMIEL":
+case "TODO JARNO":
+case "TODO LEANDER":
+case "TODO MARTIJN":
+case "TODO TOM":
+case "TODO RIANNE":
+case "TODO LISA":
+case "TODO JOOST":
+case "TODO LAURENS":
+	$wn0 = "";
+	$wn0 .= "Zoooo, ".$user_name.", wat ga jij zometeen doen? \n";
+	$wn0 .= "Jij gaat straks met een groepje van 8 collega's de Voorhoede bezoeken. ";
+	$wn0 .= "Je krijgt daar om 11:30 een (korte) rondleiding van Femke Peterson. ";
+	$wn0 .= "Het adres van de Voorhoede is: \n";
+	$wn0 .= "Rijnsburgstraat 9 - 11 \n";
+	$wn0 .= "Tel 06 4897 9447 \n";
+	$wn0 .= "Hun kantoor ligt naast de Schinkel in de buurt van het Olympisch Stadion. \n";
+	$wn0 .= "\n";
+	$wn0 .= "De Voorhoede zijn Front-end developers die zich 100% op het web richten. Ze zijn gespecialiseerd in Web technologieën: Javascript, HTML & CSS. Een goede match voor onze minor! \n";
+	$wn0 .= "In projectweek 2 komt de Voorhoede jullie een uitdagende case voorleggen. ";
+	$wn0 .= "Vraag vooral ook nog even naar de opdracht die de Voorhoede heeft gedaan voor CommTrack. Over Web technologieën gesproken ...  \n";
+	$wn0 .= $bezoek;
+break;
 
-
-	case "12345":
-		$talk .= "Hey, ".$user_name." you've finished the first Weekly Nerd. Good for you! \n";
-		$talk .= "To complete the first assignment, write a (short) blog post about the talk ";
-		$talk .= "and follow up on 2 or 3 links the speaker discussed. \n";
-		$talk .= "- This was the first Weekly Nerd (1/18) \n";
-		break;
+case "TODO KOOP":
+	$wn0 = "";
+	$wn0 .= "Geen dak boven je hoofd? Niet getreurd ".$user_name.", we gaan naar Funda. \n";
+	$wn0 .= "Je krijgt daar om 11:30 een (korte) rondleiding van Jeroen Mulder \n";
+	$wn0 .= "Het adres van Funda is: \n";
+	$wn0 .= "Piet Heinkade 167-173 \n";
+	$wn0 .= "tel 020 561 8200 \n";
+	$wn0 .= "Kwartiertje fitesen richting het IJ. \n";
+	$wn0 .= "\n";
+	$wn0 .= $bezoek;
+break;
 */
+//$talk = $wn0;
 
+
+//ECHO RESPONSE
+echo $talk;
 
 ?>
